997a483ed554dd5ab9ae2623c16798a4a6df8be4
Thiago Yamamoto <thiagoyama@gmail.com>
2023-08-04 22:54:56 -0300

    C#

---
+++ Fiap.HelloWorld/Fiap.HelloWorld.UI/Models/Cliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.HelloWorld.UI.Models
+{
+    internal class Cliente : Pessoa
+    {
+        //Atributos, fields
+        private string? _nome;
+        private int _idade;
+
+        //Propriedades Getters/Setters
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        public int Idade
+        {
+            get { return _idade; }
+            set { _idade = value; }
+        }
+    }
+}
+
+//Tarefa: criar a classe com herança e o atributo
+//Funcionario
+//Cargo
---
+++ Fiap.HelloWorld/Fiap.HelloWorld.UI/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Fiap.HelloWorld.UI.Models;
+
+Console.WriteLine("Hello, World!");
+
+//Instanciar um Cliente
+Cliente cliente = new Cliente();
+
+//Setar o nome
+cliente.Nome = "Nelson Ronqui";
+
+//Exibir o nome
+Console.WriteLine(cliente.Nome);
