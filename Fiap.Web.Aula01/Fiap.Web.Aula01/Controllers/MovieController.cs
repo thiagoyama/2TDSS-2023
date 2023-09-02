f169a281a28bbc266b3631e4488731fbea1f5634
Thiago Yamamoto <thiagoyama@gmail.com>
2023-09-01 22:46:48 -0300

    ASP.NET Controller

--- Fiap.Web.Aula01/Fiap.Web.Aula01/Controllers/MovieController.cs
+++ Fiap.Web.Aula01/Fiap.Web.Aula01/Controllers/MovieController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fiap.Web.Aula01.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fiap.Web.Aula01.Controllers
 {
     public class MovieController : Controller
     {
+        [HttpGet] //Abrir a página HTML com o formulário
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost] //Receber os parametros do formulario após o submit
+        public IActionResult Index(Movie movie)
+        {
+            //Enviar o nome do filme para a página
+            ViewData["churros"] = movie.Nome;
+            ViewBag.mochila = movie;
+            return View(); 
+            //Envia um texto para o browser
+            //return Content($"Nome: {movie.Nome}, Ano: {movie.Ano}," +
+            //    $" Faturamento: {movie.Faturamento}");
         }
     }
 }
---
+++ Fiap.Web.Aula01/Fiap.Web.Aula01/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace Fiap.Web.Aula01.Models
+{
+    public class Movie
+    {
+        public string? Nome { get; set; }
+        public int Ano { get; set; }
+        public decimal Faturamento { get; set; }
+    }
+}
