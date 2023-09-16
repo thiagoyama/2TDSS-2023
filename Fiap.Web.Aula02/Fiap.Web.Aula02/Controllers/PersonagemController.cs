197ee917ba0ecda90320af9d359f16434be3928b
Thiago Yamamoto <thiagoyama@gmail.com>
2023-09-15 22:49:55 -0300

    Views

---
+++ Fiap.Web.Aula02/Fiap.Web.Aula02/Controllers/PersonagemController.cs
@@ -0,0 +1,33 @@
+﻿using Fiap.Web.Aula02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Aula02.Controllers
+{
+    public class PersonagemController : Controller
+    {
+        //Armazenar os personagens em memória
+        private static List<Personagem> _lista = new List<Personagem>();
+
+        //Listar os personagens (Criar uma página com uma tabela HTML)
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //Enviar a lista de personagem para a view
+            return View(_lista);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Personagem personagem)
+        {
+            _lista.Add(personagem);
+            ViewBag.mensagem = "Personagem cadastrado!";
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
--- Fiap.Web.Aula02/Fiap.Web.Aula02/Models/Personagem.cs
+++ Fiap.Web.Aula02/Fiap.Web.Aula02/Models/Personagem.cs
@@ -1,11 +1,17 @@
-﻿namespace Fiap.Web.Aula02.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fiap.Web.Aula02.Models
 {
     public class Personagem
     {
         public int Id { get; set; }
-        public string? Nome { get; set; }        
+        public string? Nome { get; set; }
+
+        [Display(Name ="Nível de Energia")] //Define o label
         public int NivelEnergia { get; set; }
         public bool Capa { get; set; }
+
+        [DataType(DataType.Date), Display(Name = "Data de Criação")] //Ajusta o type do input
         public DateTime DataCriacao { get; set; }
     }
 }
