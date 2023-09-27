957747c26cfd54164ada1798972011eec16aef68
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-09-26 22:38:59 -0300

    Exercicio

---
+++ Fiap.Web.Aula02/Fiap.Web.Aula02/Models/Equipamento.cs
@@ -0,0 +1,15 @@
+﻿namespace Fiap.Web.Aula02.Models
+{
+    public class Equipamento
+    {
+        public int Codigo { get; set; }
+        public string? Nome { get; set; }
+        public Raridade Raridade { get; set; }
+        public string? Descricao { get; set; }
+    }
+
+    public enum Raridade
+    {
+        Comum, Incomum, Raro, Mitico, Lendario
+    }
+}
