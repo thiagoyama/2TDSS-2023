55b31e66be4e52ae5754fdf72f275b69b5fccd02
Thiago Yamamoto <thiagoyama@gmail.com>
2023-09-29 22:47:26 -0300

    Views

---
+++ Fiap.Web.Aula02/Fiap.Web.Aula02/TagHelpers/AlertTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Fiap.Web.Aula02.TagHelpers
+{
+    public class AlertTagHelper : TagHelper
+    {
+        public string? Mensagem { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!string.IsNullOrEmpty(Mensagem))
+            {
+                output.TagName = "div";
+                output.Attributes.SetAttribute("class", "alert alert-success");
+                output.Content.SetContent(Mensagem);
+            }
+        }
+    }
+}
---
+++ Fiap.Web.Aula02/Fiap.Web.Aula02/TagHelpers/ChurrosTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Fiap.Web.Aula02.TagHelpers
+{
+    public class ChurrosTagHelper : TagHelper
+    {
+        public string? Texto { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+            //Setar o class
+            output.Attributes.SetAttribute("class", "btn btn-primary");
+            //Setar o conteúdo do botão
+            output.Content.SetContent(string.IsNullOrEmpty(Texto)?"Cadastrar":Texto);
+        }
+    }
+}
