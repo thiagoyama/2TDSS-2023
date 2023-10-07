f52544da2934ecdb76a2442ad09a9162fbf16b2c
Thiago Yamamoto <thiagoyama@gmail.com>
2023-10-06 22:50:59 -0300

    CRUD EF

--- Fiap.Web.Aula03/Fiap.Web.Aula03/Controllers/PacienteController.cs
+++ Fiap.Web.Aula03/Fiap.Web.Aula03/Controllers/PacienteController.cs
@@ -1,10 +1,11 @@
-﻿using Fiap.Web.Aula03.Persistencia;
+﻿using Fiap.Web.Aula03.Models;
+using Fiap.Web.Aula03.Persistencia;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fiap.Web.Aula03.Controllers
 {
     public class PacienteController : Controller
-    {
+    {        
         private HospitalContext _context;
 
         //Recebe o DbContext por injeção de dependência
@@ -13,11 +14,29 @@ namespace Fiap.Web.Aula03.Controllers
             _context = context;
         }
 
-        //Tarefa é criar o CRUD com o Banco
-
-        public IActionResult Index()
+        //Criar o cadastro de paciente (Criar o método GET para abrir a página com o form HTML)
+        //Criar uma página separada com o formulário HTML (cadastrar e no editar)
+        [HttpGet]
+        public IActionResult Cadastrar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Paciente paciente)
+        {
+            //Cadastrar no banco de dados
+            _context.Pacientes.Add(paciente);
+            _context.SaveChanges();
+            //Mensagem de sucesso
+            TempData["msg"] = "Paciente cadastrado!";
+            return RedirectToAction("cadastrar");
+        }
+
+        public IActionResult Index()
+        {                      
+            var lista = _context.Pacientes.ToList();
+            return View(lista);
         }
     }
 }
