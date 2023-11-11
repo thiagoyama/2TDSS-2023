91b9f2a122624604140cf2c4c4421698b7d0527f
Thiago Yamamoto <thiagoyama@gmail.com>
2023-11-10 21:48:57 -0300

    API

---
+++ Fiap.Web.Api.Aula04/Fiap.Web.Api.Aula04/Persistencia/EscolaContext.cs
@@ -0,0 +1,16 @@
+﻿using Fiap.Web.Api.Aula04.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiap.Web.Api.Aula04.Persistencia
+{
+    public class EscolaContext : DbContext
+    {
+        public DbSet<Escola> Escolas { get; set; }
+
+        public EscolaContext(DbContextOptions op) : base(op)
+        {
+            
+        }
+
+    }
+}
---
+++ Fiap.Web.Api.Aula04/Fiap.Web.Api.Aula04/Program.cs
@@ -0,0 +1,29 @@
+using Fiap.Web.Api.Aula04.Persistencia;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var conexao = builder.Configuration.GetConnectionString("conexao");
+builder.Services.AddDbContext<EscolaContext>(o => o.UseSqlServer(conexao));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
